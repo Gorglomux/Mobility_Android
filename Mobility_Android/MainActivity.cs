878bfe8ec730bb9f6a1c5581ce9771978a73fc92
Enzo Aleixo-Carvahlo <Aleixo-Carvahlo.Enzo@ceritar.com>
2019-04-18 11:35:45 -0400

    Complete Wireframes

--- Mobility_Android/MainActivity.cs
+++ Mobility_Android/MainActivity.cs
@@ -13,7 +13,7 @@ namespace Mobility_Android
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.frmHome);
+            SetContentView(Resource.Layout.frmCreationPalette);
 
             var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetActionBar(toolbar);
