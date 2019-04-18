1e96b9a6b4254b5f13258f74622e03a2647aadd0
Armand Alleaume <Alleaume.Armand@ceritar.com>
2019-04-18 09:15:51 -0400

    initial commit

---
+++ Mobility_Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+namespace Mobility_Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/ApplicationTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.frmHome);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetActionBar(toolbar);
+            ActionBar.Title = "";
+        }
+    }
+}
